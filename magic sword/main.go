da161b85818dd76aefdf92c682df54494dbd4353
wushili <wushili@ubuntu>
2019-12-05 00:44:00 -0500

    combatServer

--- magic sword/main.go
+++ magic sword/main.go
@@ -1,41 +1,44 @@
 package main
 
 import (
+	"fmt"
+	"time"
+
 	"github.com/gin-gonic/gin"
 )
 
-type action struct {
-	underAttack bool
-	timestamp   int64
-	damage      int
+type response struct {
+	Code    int    `json:"code"`
+	Message string `json:"message"`
+}
+
+type status struct {
+	HP           int   `json:"hp"`
+	UnderAttack  bool  `json:"underAttack"`
+	Timestamp    int64 `json:"timestamp"`
+	DamageAlert  int   `json:"damageAlert"`
+	InCombat     bool  `json:"inCombat"`
+	CombatTarget int32 `json:"combatTarget"`
+	NumWin       int   `json:"numWin"`
+	NumLose      int   `json:"numLose"`
 }
 
 type player struct {
 	ID     int32  `json:"id"`
 	Name   string `json:"name"`
-	HP     int    `json:"hp"`
 	Geo    string `json:"geo"`
-	Combat bool   `json:"combat"`
-	status action
+	Status status `json:"status"`
 }
 
 type request struct {
-	User      player `json:"user"`
-	Mode      bool   `json:"mode"`
-	Target    int32  `json:"target"`
-	Damage    int    `json:"damage"`
-	Timestamp int64  `json:"timestamp"`
+	User        player   `json:"user"`
+	StartCombat bool     `json:"startCombat"`
+	Target      int32    `json:"target"`
+	Damage      int      `json:"damage"`
+	Res         response `json:"res"`
 }
 
-type playList struct {
-	Players []string `json:"players"`
-}
-
-type response struct {
-	Message string `json:"message"`
-}
-
-const reactTime = 15
+const reactTime int64 = 15
 
 var userMAP map[int32]player
 var combactMAP map[string][]player
@@ -46,6 +49,7 @@ func main() {
 	r := gin.Default()
 	r.GET("/v1/normal", normal)
 	r.GET("/v1/combat", combat)
+	r.GET("/v1/statics", getStats)
 	r.Run(":11000")
 }
 
@@ -53,44 +57,71 @@ func combat(c *gin.Context) {
 	var r request
 	err := c.BindJSON(&r)
 	if err != nil {
-		c.JSON(500, response{Message: "invalid request"})
+		c.JSON(500, request{Res: response{Code: 0, Message: "Invalid request"}})
 		return
 	}
-	if !userMAP[r.Target].Combat || !r.User.Combat {
-		c.JSON(500, response{Message: "invalid request"})
+	if r.Target == r.User.ID {
+		c.JSON(500, response{Message: "cannot target self"})
+		return
+	}
+
+	if !userMAP[r.Target].Status.InCombat {
+		c.JSON(500, response{Message: "target not in battle"})
+		return
+	}
+
+	if !userMAP[r.User.ID].Status.InCombat {
+		c.JSON(500, response{Message: "user not in battle"})
 		return
 	}
 
 	if r.Damage > 0 {
 		p := userMAP[r.Target]
-		p.status.underAttack = true
-		p.status.timestamp = r.Timestamp
-		p.status.damage = p.status.damage + r.Damage
+		p.Status.UnderAttack = true
+		p.Status.Timestamp = time.Now().Unix()
+		p.Status.DamageAlert = p.Status.DamageAlert + r.Damage
 		userMAP[r.Target] = p
-	} else {
+	} else if r.Damage < 0 {
 		p := userMAP[r.User.ID]
-		p.status.underAttack = false
-		p.status.timestamp = r.Timestamp
-		p.status.damage = p.status.damage + r.Damage
-		p.HP = r.User.HP - p.status.damage
+		p.Status.UnderAttack = false
+		p.Status.Timestamp = time.Now().Unix()
+		p.Status.HP = p.Status.HP - (p.Status.DamageAlert + r.Damage)
+		p.Status.DamageAlert = 0
+		if p.Status.HP <= 0 {
+			p.Status.InCombat = false
+			r.Res = response{Code: 1, Message: "you lose"}
+			target := userMAP[r.Target]
+			target.Status.InCombat = false
+			target.Status.NumWin = target.Status.NumWin + 1
+			userMAP[r.Target] = target
+			p.Status.NumLose = p.Status.NumLose + 1
+		}
 		userMAP[r.User.ID] = p
-		r.User.HP = p.HP
+		r.User = p
 		c.JSON(200, r)
 		return
 	}
 
 	p2 := userMAP[r.User.ID]
-	if p2.status.underAttack && p2.status.timestamp+reactTime > r.Timestamp {
-		p2.status.underAttack = false
-		p2.status.timestamp = r.Timestamp
-		p2.status.damage = 0
-		p2.HP = r.User.HP - p2.status.damage
+	if p2.Status.UnderAttack && p2.Status.Timestamp+reactTime > time.Now().Unix() {
+		fmt.Println("---------------------------------------------")
+		p2.Status.UnderAttack = false
+		p2.Status.Timestamp = time.Now().Unix()
+		p2.Status.HP = p2.Status.HP - p2.Status.DamageAlert
+		p2.Status.DamageAlert = 0
+		if p2.Status.HP <= 0 {
+			p2.Status.InCombat = false
+			r.Res = response{Code: 1, Message: "you lose"}
+			p2.Status.NumLose = p2.Status.NumLose + 1
+			target := userMAP[r.Target]
+			target.Status.InCombat = false
+			target.Status.NumWin = target.Status.NumWin + 1
+			userMAP[r.Target] = target
+		}
 		userMAP[r.User.ID] = p2
-		r.User.HP = p2.HP
-		c.JSON(200, r)
-		return
 	}
-
+	r.User = p2
+	fmt.Println(userMAP)
 	c.JSON(200, r)
 }
 
@@ -98,25 +129,32 @@ func normal(c *gin.Context) {
 	var r request
 	err := c.BindJSON(&r)
 	if err != nil {
-		c.JSON(500, response{Message: "invalid request"})
+		c.JSON(500, request{Res: response{Code: 0, Message: "Cannot Parse Request"}})
 		return
 	}
 	// check if is in battle status
 	p, ok2 := userMAP[r.User.ID]
-	if p.Combat {
-		r.User.Combat = true
+	if p.Status.InCombat {
+		r.User = p
+		r.Res = response{Code: 1, Message: "battle start"}
 		c.JSON(200, r)
 		return
 	}
 	// check if the device wants to start combat
-	if r.User.Combat {
+	if r.StartCombat {
 		player, ok := userMAP[r.Target]
 		if ok {
-			player.Combat = true
+			player.Status.InCombat = true
+			player.Status.CombatTarget = r.User.ID
 			userMAP[r.Target] = player
+			p.Status.InCombat = true
+			p.Status.CombatTarget = r.Target
+			userMAP[r.User.ID] = p
+			r.Res = response{Code: 1, Message: "battle start"}
+			r.User = p
 			c.JSON(200, r)
 		} else {
-			c.JSON(200, response{Message: "Invalid Target"})
+			c.JSON(200, request{Res: response{Code: 0, Message: "invalid target"}})
 		}
 		return
 	}
@@ -147,8 +185,23 @@ func normal(c *gin.Context) {
 			}
 		}
 		combactMAP[p.Geo] = playerList2
+		r.User.Status.NumWin = p.Status.NumWin
+		r.User.Status.NumLose = p.Status.NumLose
 	}
+	r.User.Status.HP = 100
+	r.User.Status.UnderAttack = false
+	r.User.Status.InCombat = false
 	userMAP[r.User.ID] = r.User
 	combactMAP[r.User.Geo] = updatedPlayers
 	c.JSON(200, updatedPlayers)
+}
+
+func getStats(c *gin.Context) {
+	var r request
+	err := c.BindJSON(&r)
+	if err != nil {
+		c.JSON(500, request{Res: response{Code: 0, Message: "Cannot Parse Request"}})
+		return
+	}
+	c.JSON(200, userMAP[r.User.ID])
 }
